2e89e652fa66731c5fe64c04001ef7a973f6436a
Justin Wright <djw6497@gmail.com>
2023-08-10 17:46:42 -0500

    adding styling for movies list, chores, and adding new books

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "New Book Releases";
+   let book1 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpFjZaDbhPjNPEHHgos3kOlWz5_TZ8fNfAEw&usqp=CAU";
+   let book2 = "https://images.csmonitor.com/csmarchives/2010/11/13831001.JPG?alias=standard_900x600";
+   let book3 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFn1-migsaqnWRplRu_CuWmblxp4Ruro0HEQ&usqp=CAU";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Breaking Dawn" />
+         <img src={book2} alt="Harry Potter and the Deathly Hallows" />
+         <img src={book3} alt="The Outsider" />
       </div>      
    );
 }
